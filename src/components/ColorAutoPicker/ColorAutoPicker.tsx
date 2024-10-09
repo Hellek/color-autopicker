f2b6f4aed18707cc2f1ce95231f11729c9615322
Roman Basharin <r.basharin@vk.team>
2024-10-09 12:58:31 +0300

    Remove dead images

--- src/components/ColorAutoPicker/ColorAutoPicker.tsx
+++ src/components/ColorAutoPicker/ColorAutoPicker.tsx
@@ -36,9 +36,6 @@ const imagesList: ImageRecord[] = [
     name: 'zmvk', src: 'https://s3-alpha-sig.figma.com/img/3491/d8d4/762599017a0997f52227072412d9d54d?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GrgUiXGd-cXZ6p8gVkPy3UYls3eSpy1NXStMpDVRNb6VeNpj6R8CuqWKFbWLejaH00-5TiBxMGD6IY65U02TM-ALcQwwbNTtCraGjPWmWdDgd2HzPdjbVW7EB~sSRhj2cV1smy6N9ncZ5dUixes90cfVPSTTotlAVPmh7SG46wU7PCjNG789tpAp~0fzOOBDw-8YkCTUzYJ4pT7RwnxJ-ZGCcIqxKQnTbCcg7Aog4ZdycwAgFnD7s4q76~Woteg01Yo~VpiHB4e8x0dSIlaEn9Qs-g9f9UYkXGX1leE5vHdHWUdS~9Gv9byqWgMWtmJ1ManhIxyWrx6h-TDkE-mc2w__',
   },
   {
-    name: 'bondar', src: 'https://s3-alpha-sig.figma.com/img/f1d0/1519/6a5cf6136dde95977fbba232211ce8b0?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qeXNpqF~gsf2244CLTcjmJ4icD6ZsDpQMoQncamJb0IYIJFWGZVjLhivs62oZXHjDKgY51rxqJUVzGWRA1aG0oKTVeFoNgBrP8yF6oMYZIOnWbtrnubJ0H-z2FnoVhmenLFlR~f2ODvfWB0TaAzaImRqUV2K9IsqhGM00qJ2Ukwtb~XuDjC0i888n1Aaf7tZ8GOtAGpLz4Dzp7Y76hp2Ux1U5ACqcqX4d~6Imkdrb43ydZHz~mZq6Xv5dGDurJC35dhHkplVJFcE1wjB-P9NFifZMIRksdC7j1S8sBrRLcyRoUwbBk4Ze04WAR-SQre9UcsukAkBIO03O3WWDk0QLg__',
-  },
-  {
     name: 'tanks', src: 'https://s3-alpha-sig.figma.com/img/bbef/7c95/7c207556e213bcdc26c64b14df6f79f4?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l88LRe3CS5lZlROAnH3bBOFRMwXSLDkAvj1HEcsRcp~~3sHNuEsf96yuNOqYrEB0YBP0EvkQmFf5rGfXnCFPQ2LnKcy9d~OzByX2Zff1ozzszeBDR8wW3u2slhwa5bnH4VYoVxdbSh0-pBgaOuFIXjcY4vXqodXdqn2XikPCF7ztQkt8ywpsWDSn-~hdr4tICpSruQ50VH0lzC1kVtiVMg7jFwIrKHDG3IrI9MiLnJsTrWffIUnRvglUWIncFbKgLiPBxpRI6fNvAnUMG2QfLUs4YG4YZXIya~k-AAfc3ZQzICny14~TEIieh~e8IOynknuzmMKgoCpZ2Yg6VaNhNA__',
   },
   {
@@ -79,9 +76,6 @@ const imagesList: ImageRecord[] = [
   },
   {
     name: 'litres', src: 'https://s3-alpha-sig.figma.com/img/6687/46a1/117ea201f09e3a2a8d61e5c3ddbc08ee?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GkK9ecHnhucI5qLbosv6Q8HJPlfBqE8zeyyJ9pkZDv5QOKGc4Us-QWC87GLZdUxmpfiSMrAxKZVHmmXvONEbH0HKkfsC2kIzyMmG9zM5IK3jMeWE36eGKlci55jf6kjgurdzsJedib3~twAs3vOO3Sch7z-MyH2tqpxieJCw7AwJ34MNm0XVbMIhVOtJhJiHsjNjvMI-1pxR7WXRs1nxRZRdl9eeYImNEuse0pG6YUUavy-mT1wk7dl3Agjy~H-CCTErYP6CnnUpnOfYbFCFPUD7n5-jLGPWY3tWzKY~3yuQwuOHjiCCp6qGA0LzZEgBULX2TaSdDxnKrZ6RljWxrw__',
-  },
-  {
-    name: 'stol food', src: 'https://s3-alpha-sig.figma.com/img/71c8/7996/b2bd1f072fb0c3b1f9f4e8daeac9f3e0?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XRdBr26zlkmD-MPK6sfV0fqX9C6-aG9gIL5dChUZ5hm269PdQlnaXVvgLQNt6~CnBpIwkcpYylgcVZfqdEmTuHZnQvOitckjCDcribz4MtiXN-LpyDg6UA5BpC98pQFlbQPZpzZDMc4e-RjgMmAZhOAW3A~vcr1eJOO1V5AXydOW44mtxtYizp2sAt-iDJbktl9ioQpw22XKsWKrKZxHVDGXOt4dWPY5AMzyTjzsCsyDl-VHdqcnk9FgQWrYO6XtgY~tMWCWNKTjLQHR4l8c0AOl7R7ZgpS81NWyEiReNuF7GIDfGlWVHhsiAHZACZAKKba00RVB-hluviB17Qg3-Q__',
   },
   {
     name: 'mvideo', src: 'https://s3-alpha-sig.figma.com/img/df68/2915/87377d22d529a8f3264054bf66581962?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gY-pQ4B7pH1s85m6KJOCJVvZ~peYupmudffUYFMpCf8u23uSbQ2th07xEuz2j8qrhSikDXVr~zJgPNTKRH7g661sovstfiWdiiUTm98xMxQrX5FLHEWBfkK-5OH8tLQ6s-u0aJkbX4NaBZ112kNML9TqQfTd5F8vacBgS1pS70Te0Lq9F~IXNtQlfzXtpE7qZOGm56yFFPCM0v4l3IqFOdSbZ9uB3heaci6eGfHxRwdowaOWem0Qk7Yx64l5MQJSjImRbuDxURo7HGSYRNBHTLKiEpnNMMwU6BPgv6Ay2UONciYEJpWka-oSm~HwRVIFAhkstc7U5hHDSrSDVd7fzw__',
