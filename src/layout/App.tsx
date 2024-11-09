1fed107b095f0103da88e0062d50b2ddab2112c1
Roman Basharin <r.basharin@vk.team>
2024-11-09 18:55:25 +0300

    Fix header mobile layout

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -12,50 +12,47 @@ export const App = () => {
 
   return (
     <DefaultLayout loading={false}>
-      <div className="py-4 px-1 flex flex-wrap items-center justify-between">
-        <div className="flex flex-wrap items-center gap-6">
-          <label className="flex">
-            <input
-              type="checkbox"
-              checked={palette}
-              onChange={() => setPalette(!palette)}
-            />
-            Show palette
-          </label>
+      <div className="py-4 px-1 flex flex-wrap items-center gap-x-6 gap-y-3">
+        <label className="flex">
+          <input
+            type="checkbox"
+            checked={palette}
+            onChange={() => setPalette(!palette)}
+          />
+          Show palette
+        </label>
 
-          <label className="flex">
-            <input type="number" className="max-w-[32px] mr-2" value={colorAmount} onChange={e => setColorAmount(Number(e.target.value))} />
-            Max palette size
-          </label>
+        <label className="flex">
+          <input type="number" className="max-w-[32px] mr-2" value={colorAmount} onChange={e => setColorAmount(Number(e.target.value))} />
+          Max palette size
+        </label>
 
-          <label className="flex">
-            <input type="number" className="max-w-[32px] mr-2" value={colorGroup} onChange={e => setColorGroup(Number(e.target.value))} />
-            Palette algo sensivity
-          </label>
-        </div>
-        <div>
-          <label className="flex">
-            <span className="bg-green-800 hover:bg-green-700 px-2 py-1 rounded-md cursor-pointer">Download images</span>
-            <input
-              type="file"
-              className="hidden"
-              multiple
-              onChange={e => {
-                if (e.target.files) {
-                  const { files } = e.target
-                  const sources: string[] = []
+        <label className="flex">
+          <input type="number" className="max-w-[32px] mr-2" value={colorGroup} onChange={e => setColorGroup(Number(e.target.value))} />
+          Palette algo sensivity
+        </label>
 
-                  for (let index = 0; index < files.length; index++) {
-                    const f = files[index]
-                    sources.push(URL.createObjectURL(f))
-                  }
+        <label className="flex">
+          <span className="bg-green-800 hover:bg-green-700 px-2 py-1 rounded-md cursor-pointer">Download images</span>
+          <input
+            type="file"
+            className="hidden"
+            multiple
+            onChange={e => {
+              if (e.target.files) {
+                const { files } = e.target
+                const sources: string[] = []
 
-                  setimageSources(sources)
+                for (let index = 0; index < files.length; index++) {
+                  const f = files[index]
+                  sources.push(URL.createObjectURL(f))
                 }
-              }}
-            />
-          </label>
-        </div>
+
+                setimageSources(sources)
+              }
+            }}
+          />
+        </label>
       </div>
 
       {imageSources.length > 0 && (
